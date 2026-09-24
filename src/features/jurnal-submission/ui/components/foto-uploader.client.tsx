b5d29525b5dc41cbfae3d3358ed3a011bbfd3db5
MohammadNasrullohh <srullasrul59@gmail.com>
2026-09-24 14:35:03 +0700

    revert: remove 1MB client-side file limit to rely on backend server limit

--- src/features/jurnal-submission/ui/components/foto-uploader.client.tsx
+++ src/features/jurnal-submission/ui/components/foto-uploader.client.tsx
@@ -20,13 +20,6 @@ export function FotoUploader({ value, onChange }: Props) {
     setError(null)
     const file = e.target.files[0]
 
-    // Validasi ukuran maksimal 1MB
-    if (file.size > 1 * 1024 * 1024) {
-      setError('Ukuran foto maksimal 1MB!')
-      e.target.value = ''
-      return
-    }
-
     // Reset input
     e.target.value = ''
 
