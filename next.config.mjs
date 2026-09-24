ddce9d42f90633052cc82bcbad36db85d18fc261
MohammadNasrullohh <srullasrul59@gmail.com>
2026-09-24 11:01:21 +0700

    fix: increase server actions body size limit to 50mb to allow large image uploads

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '50mb',
+    },
+  },
   output: 'standalone',
   async headers() {
     return [
